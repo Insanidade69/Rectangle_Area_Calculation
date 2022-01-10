a6490ca31f22c22577999a515cdab0cbd85d18bb
Marcelo <mvinicius.louza@gmail.com>
2022-01-10 19:46:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rectangle_Area_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle();
+
+            Console.WriteLine("-=-=-=-=-=-=-= Rectangle Area =-=-=-=-=-=-=-");
+
+            Console.WriteLine("Enter a height and width of rectangle!");
+            Console.Write("A: ");
+            rec.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("B: ");
+            rec.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.WriteLine(rec.ToString());
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rectangle_Area_Calculator
+{
+    class Rectangle
+    {
+        public double A;
+        public double B;
+
+        public double Area()
+        {
+            return A * B;
+        }
+        public double Perimeter()
+        {
+            return 2 * (A + B);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+        }
+        public override string ToString()
+        {
+            return "Area is equals: " + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPerimeter is equals: " + Perimeter().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDiagonal is equals: " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
